bb9004fe9cb6b3cf71073713d26a0d76bc8ae6d7
zeze.li <lizeze.boy@gmail.com>
2021-03-07 16:11:06 +0800

    增加css和image引用

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
-let el:HTMLDivElement = document.createElement("div");
-el.innerHTML="webpack and ts";
-document.body.append(el)
+import "./index.css";
+import img from "../img/start.png";
+let el: HTMLDivElement = document.createElement("div");
+el.classList.add("hello");
+el.innerHTML = "webpack and ts";
+document.body.append(el);
+
+let image: HTMLImageElement = document.createElement("img");
+
+image.src = img;
+document.body.append(image);
--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,37 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
-    mode:"development",
+    mode: "development",
     entry: './src/index.ts',
     module: {
         rules: [{
-            test: /\.tsx?$/,
-            use: 'ts-loader',
-            exclude: /node_modules/,
-        }, ],
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use:['file-loader'],
+            },
+        ],
     },
     resolve: {
         extensions: ['.tsx', '.ts', '.js'],
     },
     plugins: [
         new HtmlWebpackPlugin({
-          title: 'Output Management',
+            title: 'Output Management',
         }),
-      ],
+    ],
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: '[name].js',
-    },devServer: {
+    },
+    devServer: {
         contentBase: './dist',
-      },
+    },
 };
