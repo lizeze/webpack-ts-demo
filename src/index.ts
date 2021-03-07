1c91be62c83c116ed47b70e78e731e1fca67ad90
zeze.li <lizeze.boy@gmail.com>
2021-03-07 16:17:08 +0800

    修改格式

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,6 @@ let el: HTMLDivElement = document.createElement("div");
 el.classList.add("hello");
 el.innerHTML = "webpack and ts";
 document.body.append(el);
-
 let image: HTMLImageElement = document.createElement("img");
-
 image.src = img;
 document.body.append(image);
